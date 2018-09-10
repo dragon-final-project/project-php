f2c684d51c2b971f51b7580b37cf08df18ef9d5a
teacher144123 <teacher144123@gmail.com>
2018-09-10 12:41:46 +0800

    rename fix h2

--- all_recipe.php
+++ all_recipe.php
@@ -3,7 +3,7 @@
   $test = include('./connect.php');
 
   // all recipes
-  echo '<h3>Ingredients: </h3>';
+  echo '<h2>All recipes: </h3>';
   $sql = "SELECT * FROM `recipe`";
   $res = $db->prepare($sql);
   $res->execute();
@@ -15,7 +15,7 @@
     echo "<tr>";
     echo "<td>" . $row['id'] . "</td>";
     echo "<td>" . $row['title'] . "</td>";
-    echo "<td><a href='test.php?r_id=" . $row['id'] . "'>Click me</a></td>";
+    echo "<td><a href='get_one_recipe.php?r_id=" . $row['id'] . "'>Click me</a></td>";
     echo "</tr>";
   }
   echo "</table>";
